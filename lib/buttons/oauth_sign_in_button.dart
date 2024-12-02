a8f8fc8066d6a24b7bf50319d8dfe166d96965e5
cyberpwn <danielmillst@gmail.com>
2024-12-02 18:59:52 -0500

    OAuth sign in impl

--- lib/buttons/oauth_sign_in_button.dart
+++ lib/buttons/oauth_sign_in_button.dart
@@ -1,12 +1,18 @@
 import 'package:arcane/arcane.dart';
+import 'package:arcane_auth/arcane_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:serviced/serviced.dart';
 
-class OAuthSignInButton extends StatelessWidget {
+class CredentialSignInButton extends StatelessWidget {
   final String? label;
   final VoidCallback onPressed;
   final Widget? icon;
 
-  const OAuthSignInButton(
+  const CredentialSignInButton(
       {super.key, required this.onPressed, this.icon, required this.label});
+
+  Future<void> signInWithCredential(AuthCredential credential) =>
+      svc<AuthService>().signIn(credential);
 
   @override
   Widget build(BuildContext context) {
